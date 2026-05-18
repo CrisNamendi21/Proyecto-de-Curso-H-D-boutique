318b4b4841fbf9d948d15a95d774641eb2c193de
Cristopher <cristofernamendi16@gmail.com>
2026-05-18 15:16:10 -0600

    archivos base (vacios) para depar, municipio, talla y pago

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import URL
 
+#Importamos el objeto settings() del fichero config.py
 from app.config import settings
 
-
+#Se construye la URL de conexion
 connection_url = URL.create(
     "mssql+pyodbc",
     host=settings.DB_SERVER,
@@ -15,17 +16,23 @@ connection_url = URL.create(
     }
 )
 
+#Creacion del motor de base de datos
 engine = create_engine(connection_url)
 
+#Se crean las sesiones
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+#se define esta linea de codigo
 Base = declarative_base()
 
+#Base es importante porque todos los modulos de SQLAlchemy heredan de ahi
 
+#Esta funcion abre una sesion de base de datos, se la entrega a las rutas y luego
+#la cierra automaticamente
 def get_db():
     db = SessionLocal()
 
